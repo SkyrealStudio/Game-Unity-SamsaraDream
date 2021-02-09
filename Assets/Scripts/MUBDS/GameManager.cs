25651a0afb320af074912c44f9297c0f76ca825a
ycMia <zzlyx20123201@126.com>
2021-02-09 08:15:14 +0800

    updated Memory-Unity-BeyondDiary-Scripts

---
+++ Assets/Scripts/MUBDS/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Unity Calls
+    /*----- Unity Calls -----*/
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+    #endregion
+}
---
+++ Assets/Scripts/MUBDS/README.md
@@ -0,0 +1,6 @@
+# Beyond Diary
+---
+> Beyond Diary, it's Memory.  
+---
+### Build Progress:
+> Started short before, currently build basic structures.
