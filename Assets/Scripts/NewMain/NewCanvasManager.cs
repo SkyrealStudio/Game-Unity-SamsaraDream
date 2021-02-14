3baa9a555109278c0b4488287d058cfd062f089e
TianC Wang <thewater2016@qq.com>
2021-02-14 23:02:32 +0800

    Final Push 20210214

--- Assets/Scripts/NewMain/NewCanvasManager.cs
+++ Assets/Scripts/NewMain/NewCanvasManager.cs
@@ -1,9 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Canvas(UI) manager.
 /// </summary>
 public class NewCanvasManager : MonoBehaviour
 {
-
+    /// <summary>
+    /// Load a layout prefab.
+    /// </summary>
+    private void _loadLayout(string _LayoutName)
+    {
+        GameObject Layout = Instantiate(Resources.Load<GameObject>(_LayoutName));
+        Layout.transform.parent = transform;
+        Layout.transform.localPosition = Vector3.zero;
+        Layout.transform.localScale = Vector3.one;
+        Layout.name = _LayoutName;
+    }
+    #region Unity Calls
+    /*----- Unity Calls -----*/
+    private void Start()
+    {
+        _loadLayout("NewUILayout_1");
+    }
+    #endregion
 }
