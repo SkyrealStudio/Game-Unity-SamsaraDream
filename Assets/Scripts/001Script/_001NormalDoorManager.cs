636f21930e1936c5e9dd3c35e7dec2c4c9f89470
ycMia <zzlyx20123201@126.com>
2021-03-07 23:25:27 +0800

    playground thinking for buggy

---
+++ Assets/Scripts/001Script/_001NormalDoorManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _001NormalDoorManager : MonoBehaviour
+{
+    // closed -> false
+    public bool doorStatus = false;
+    public GameObject doorNormal;
+    public GameObject opener1;
+    public GameObject opener2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        doorStatus = doorNormal.GetComponent<BoxCollider2D>().isTrigger;
+    }
+}
